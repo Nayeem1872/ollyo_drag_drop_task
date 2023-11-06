c23e7db5b30187d0e54db0238f4a880ae52160f7
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2023-11-06 08:35:03 +0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,167 @@
-import logo from './logo.svg';
+import { BsCardImage } from "react-icons/bs";
+import { useState } from "react";
+import { imagesData } from "./data/ImagesData";
 import './App.css';
 
 function App() {
+  const [images, setImages] = useState(imagesData);
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [draggedItem, setDraggedItem] = useState(null);
+
+  const handleDragStart = (e, index) => {
+    setDraggedItem(images[index]);
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/html", e.target.parentNode);
+  };
+
+  const handleDragOver = (index) => {
+    const draggedOverItem = images[index];
+
+    if (draggedItem === draggedOverItem) return;
+
+    const imagesCopy = Array.from(images);
+    const fromIndex = imagesCopy.indexOf(draggedItem);
+    const toIndex = imagesCopy.indexOf(draggedOverItem);
+
+    imagesCopy.splice(fromIndex, 1);
+    imagesCopy.splice(toIndex, 0, draggedItem);
+
+    setImages(imagesCopy);
+  };
+
+  const handleDragEnd = () => {
+    setDraggedItem(null);
+  };
+
+ 
+
+  const handleCheckbox = (index) => {
+    const selectedImage = images[index];
+    const isImageSelected = selectedImages.some(
+      (img) => img.image === selectedImage.image
+    );
+
+    let updatedImages;
+
+    if (isImageSelected) {
+      updatedImages = selectedImages.filter(
+        (img) => img.image !== selectedImage.image
+      );
+    } else {
+      updatedImages = [...selectedImages, selectedImage];
+    }
+
+    setSelectedImages(updatedImages);
+
+    const imageContainers = document.querySelectorAll(".image-item-container");
+
+    imageContainers.forEach((container, i) => {
+      const image = container.querySelector("img");
+      const isCurrentImageSelected = updatedImages.some(
+        (img) => img.image === images[i].image
+      );
+
+      if (isCurrentImageSelected) {
+        container.classList.add("clicked");
+        image.style.filter = "blur(2.5px)";
+      } else {
+        container.classList.remove("clicked");
+        image.style.filter = "none";
+      }
+    });
+  };
+
+  const deleteSelectedImages = () => {
+    const updatedImages = images.filter(
+      (img) => !selectedImages.some((selected) => selected.image === img.image)
+    );
+
+    const updatedSelectedImages = selectedImages.filter((selected) =>
+      updatedImages.some((img) => img.image === selected.image)
+    );
+    setSelectedImages(updatedSelectedImages);
+
+    const imageContainers = document.querySelectorAll(".image-item-container");
+
+    imageContainers.forEach((container, i) => {
+      const image = container.querySelector("img");
+      const isCurrentImageSelected = updatedSelectedImages.some(
+        (img) => img.image === images[i].image
+      );
+
+      if (isCurrentImageSelected) {
+        container.classList.add("clicked");
+        image.style.filter = "blur(5px)";
+      } else {
+        container.classList.remove("clicked");
+        image.style.filter = "none";
+      }
+    });
+
+    setImages(updatedImages);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="image-container">
+      <div className="header">
+        {selectedImages.length === 0 ? (
+          <div className="header-text">Gallery</div>
+        ) : (
+          <div className="flexContainer">
+            <div className="header-text">
+              <input
+                type="checkbox"
+                checked={true}
+                className="header-checkbox"
+              />
+              {selectedImages.length} Files Selected
+            </div>
+            <div onClick={deleteSelectedImages}>
+              <a href="#" className="header-text-Link">
+                Delete File
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
+
+      <hr />
+
+      <div className="image-grid">
+        {images.map((img, index) => (
+          <div
+            key={index}
+            onDragOver={() => handleDragOver(index)}
+            className={index < 1 ? "image-item-first" : "auto"}
+          >
+            <div
+              className="image-item-container"
+              draggable
+              onDragStart={(e) => handleDragStart(e, index)}
+              onDragEnd={handleDragEnd}
+            >
+              <img src={img.image} alt={img.image} className="image-item" />
+              <input
+                type="checkbox"
+                className="checkbox"
+                checked={selectedImages.some(
+                  (selected) => selected.image === img.image
+                )}
+                onClick={() => handleCheckbox(index)}
+              />
+            </div>
+          </div>
+        ))}
+       <label className="add-image-button">
+  <input type="file" style={{ display: 'none' }} />
+  <div style={{ display: 'flex', flexDirection: 'column', marginTop:'75px', alignItems: 'center', justifyContent: 'center' }}>
+    <h2>
+      <BsCardImage />
+    </h2>
+    <h3 className="add">Add Images</h3>
+  </div>
+</label>
+      </div>
     </div>
   );
 }
